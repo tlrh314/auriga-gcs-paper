7d8b9617877ca0e4e132420632eb16b0fb0a9b9c
Timo Halbesma <timohalbesma@gmail.com>
2019-08-01 19:15:32 +0000

    Started processing SW's feedback, but not finished

--- latexdiff.sh
+++ latexdiff.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-diff="diff_with_respect_to_20190614.tex"
+diff="diff_with_respect_to_20190712.tex"
 if [ -f "${diff}" ]; then
     rm -f "${diff}"
 fi
 
 # Ignore (sub)sections: https://tex.stackexchange.com/questions/87536
-latexdiff --exclude-textcmd="section,subsection" AurigaGCS_20190614.tex AurigaGCS.tex > "${diff}"
+latexdiff --exclude-textcmd="section,subsection" AurigaGCS_20190712.tex AurigaGCS.tex > "${diff}"
 timotex "${diff}"
