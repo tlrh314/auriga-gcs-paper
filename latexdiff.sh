13bc0ce4568c83bcb0902d1e3384309d0005faeb
Timo Halbesma <timohalbesma@gmail.com>
2019-08-05 17:00:52 +0000

    Remove Volker Springel as author b/c he has not contributed directly (his words)

--- latexdiff.sh
+++ latexdiff.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-diff="diff_with_respect_to_20190712.tex"
+diff="diff_with_respect_to_20190802.tex"
 if [ -f "${diff}" ]; then
     rm -f "${diff}"
 fi
 
 # Ignore (sub)sections: https://tex.stackexchange.com/questions/87536
-latexdiff --exclude-textcmd="section,subsection" AurigaGCS_20190712.tex AurigaGCS.tex > "${diff}"
+# NB, removed by hand Volker Springel from AurigaGCS_20180802.tex b/c
+# latexdiff breaks on changes in the \author block
+latexdiff --exclude-textcmd="section,subsection" AurigaGCS_20190802.tex AurigaGCS.tex > "${diff}"
 timotex "${diff}"
