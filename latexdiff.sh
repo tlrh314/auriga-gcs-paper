b701c46b22c48173707dc6a526da7c9b2f4cc195
Timo Halbesma <timohalbesma@gmail.com>
2019-08-23 06:54:49 +0200

    Updated latexdiff.sh to process RG's final remarks + FG feedback #1

--- latexdiff.sh
+++ latexdiff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-diff="diff_with_respect_to_20190802.tex"
+diff="diff_with_respect_to_20190820.tex"
 if [ -f "${diff}" ]; then
     rm -f "${diff}"
 fi
@@ -9,5 +9,5 @@ fi
 # Ignore (sub)sections: https://tex.stackexchange.com/questions/87536
 # NB, removed by hand Volker Springel from AurigaGCS_20180802.tex b/c
 # latexdiff breaks on changes in the \author block
-latexdiff --exclude-textcmd="section,subsection" AurigaGCS_20190802.tex AurigaGCS.tex > "${diff}"
+latexdiff --exclude-textcmd="section,subsection" AurigaGCS_20190820.tex AurigaGCS.tex > "${diff}"
 timotex "${diff}"
